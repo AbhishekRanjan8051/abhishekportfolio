cb098626a2a0851266d152291b3453c328f73419
AbhishekRanjan8051 <abhishekranjan8051@gmail.com>
2022-06-24 17:04:27 +0530

    v 2.0

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import seekho from "../images/seekho.png";
 import lovelula from "../images/lovelula.png";
+import porter from "../images/porter.png";
+import tictoetic from "../images/tic_toe_tic.png";
+import randomquotes from "../images/randomquotes.png";
 
 function Projects() {
   return (
@@ -15,8 +18,9 @@ function Projects() {
               <h5 className="card-title">Seekho App</h5>
               <img className="card_img" src={seekho} alt="no" />
               <p className="card-text">
-                This is a site made with html, css & js which simply shows the
-                weather of any place accurately.
+                This is a site made with Reactjs,nodejs,expressjs and for
+                database we use MonogDb,which provide us the various type of
+                book to raed and learn everyday newthing.
               </p>
               <a
                 target="_blank"
@@ -69,7 +73,7 @@ function Projects() {
           <div className="card">
             <div className="card-body">
               <h5 className="card-title">Porter.in</h5>
-              <img className="card_img" src={seekho} alt="no" />
+              <img className="card_img" src={porter} alt="no" />
               <p className="card-text">
                 Porter is a huge network of transportation for intra-city
                 logistics services.
@@ -96,11 +100,41 @@ function Projects() {
           </div>
           <div className="card">
             <div className="card-body">
-              <h5 className="card-title">Tic Toe Tic Game</h5>
-              <img className="card_img" src={seekho} alt="no" />
+              <h5 className="card-title">Random Quotes</h5>
+              <img className="card_img" src={randomquotes} alt="no" />
               <p className="card-text">
-                I've made this with Reactjs , this game is for two player who
-                want to play tic toe tic online unless they want waste paper.
+                This is made of html css, which provides us the worlds best
+                quotes and we can also share our quotes to social media
+                directly.
+              </p>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/AbhishekRanjan8051/Random_Quotestic_toe_tic"
+              >
+                <button type="button" className="btn btn-primary">
+                  Github
+                </button>
+              </a>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://random-quotes-lilac.vercel.app/"
+              >
+                <button type="button" className="btn btn-primary">
+                  Demo
+                </button>
+              </a>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Tic Toe Tic Game</h5>
+              <img className="card_img" src={tictoetic} alt="no" />
+              <p className="card-text">
+                I've made this with Reactjs , this is multiplayer game.this is
+                world oldest game which train your mind to make right decisions
+                at low time.
               </p>
               <a
                 target="_blank"
